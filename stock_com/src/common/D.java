03bfbfc40f52d942a6f040a12c93e8c86de2376c
DohyeonJ <jdh7693@naver.com>
2020-09-01 23:23:45 +0900

    db관련 인터페이스 D생성완료

---
+++ stock_com/src/common/D.java
@@ -0,0 +1,32 @@
+package common;
+
+public class D {
+	public static final String DRIVER = "org.mariadb.jdbc.Driver";
+	public static final String URL = "jdbc:mariadb://localhost:3306/mydb";
+	public static final String USERID = "myuser";
+	public static final String USERPW = "1234";
+		
+	//회원가입, 로그인 관련 쿼리문
+	public static final String SQL_MEMBER_INSERT =
+			"INSERT INTO st_member"
+			+"(member_uid, member_name, member_id, member_pw, member_email,"
+			+" member_gender, member_birth, member_phone)"
+			+"VALUES"
+			+"(?, ?, ?, ?, ?, ?, ?, ?)";
+	
+	//게시글 관련 쿼리문
+	public static final String SQL_BORAD_INSERT =
+			"INSERT INTO st_board"
+			+"(board_uid, board_name, board_title, board_content, board_date, board_viewcnt)"
+			+"VALUES"
+			+"(?, ?, ?, ?, ?, ?)";
+	//댓글 관련 쿼리문
+	public static final String SQL_COMMENT_INSERT =
+			"INSERT INTO st_comment"
+			+"(comment_uid, comment_con_num, comment_name, comment_pw, comment_content, comment_date)"
+			+"VALUES"
+			+"(?, ?, ?, ?, ?, ?)";
+
+}
+
+
