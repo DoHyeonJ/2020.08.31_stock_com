745f7047ec925db225bc03a8666370d01aef05f2
DohyeonJ <jdh7693@naver.com>
2020-09-04 22:37:33 +0900

    St_DAO 작성진행

---
+++ stock_com/src/com/lec/beans/St_DAO.java
@@ -0,0 +1,41 @@
+package com.lec.beans;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import common.D;
+
+public class St_DAO {
+	Connection conn;
+	PreparedStatement pstmt;
+	Statement stmt;
+	ResultSet re;
+	
+	// DAO 객체가 생성될때 Connection 도 생성됨.
+	public St_DAO() {
+		try {
+			Class.forName(D.DRIVER);
+			conn = DriverManager.getConnection(D.URL, D.USERID, D.USERPW);
+			System.out.println("St_DAO 객체 생성, 데이터베이스 연결.");
+		//클래스를 찾지 못했을때의 예외처리
+		} catch (ClassNotFoundException e) { 
+			e.printStackTrace();
+		//SQL 에러시의 예외처리
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// DB 자원 반납 메소드
+	public void close() throws SQLException {
+		if(re != null) re.close();
+		if(pstmt != null) pstmt.close();
+		if(stmt != null) stmt.close();
+		if(conn != null) conn.close();
+
+	}
+}
